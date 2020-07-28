7649225ce5645478ff47e23bc630597ca1715331
jradd <jredd42@gmail.com>
2020-07-28 00:13:54 -0600

    this is a mess of script.

--- ipy.py
+++ ipy.py
@@ -1,29 +1,91 @@
 import json
 from os import path
 import sys
-import simple_http
-
-URL = 'https://download.dnscrypt.info/dnscrypt-resolvers/json/public-resolvers.json'
-if sys.argv[1] == 'get':
-    simple_http.http_get(URL)
+from simple_http import http_get
+from public.gif3 import handler
+import re
 
 
-with open('public-resolvers.json') as f:
+   
+#URL = 'https://download.dnscrypt.info/dnscrypt-resolvers/json/public-resolvers.json'
+#with open('public-resolvers.json', 'w') as f:
+
+#    for line in http_get(URL):
+#        f.write(line)
+
+#    data = json.loads(f.read())
+
+
+ooh = handler('public-resolvers.json3', 'public-resolvers3-copy.json')
+
+#with open('public-resolvers.json', 'r') as f:
+#    d = f.read()
+#    data = json.loads(d)
+
+with open('public-resolvers.json3', 'rb+') as f:
     d = f.read()
     data = json.loads(d)
 
-newList = [list(tuple((x['country'], x['stamp'], x['proto'])))
+
+
+newList = [list(tuple((x['country'], x['stamp'], x['proto'], x['addrs'])))
            for x in data
            if x['dnssec'] and x['nofilter'] and x['nolog'] is True]
 
+filterTrue = [list(tuple((x['country'], x['stamp'], x['proto'], x['addrs'])))
+           for x in data
+           if x['dnssec'] and x['nolog'] is True]
+
+
+iplist=list()
+pattern = r'(?P<ipv4>\d{1,3}\.\d{1,3}\d{1,3}\d{1,3})'
+s = ""
+for j in range(len(newList)):
+    if 'DNSCrypt' in newList[j][2]:
+        if isinstance(newList[j][-1], str):
+            iplist.append(newList[j][-1])
+        else:
+            for ip in newList[j][3]:
+                if isinstance(ip, str):
+                    s+=f' {newList[j][0]}: {ip}\r\n'
+                    iplist.insert(j, (newList[j][0], newList[j][-1]))
+
+#m = re.search(pattern, ss)
+#print('valid\r\n', m.group(0))
+ipv4list=list()
+#entries = re.split('\n+', s)
+#ipv4=[re.split('?\w+\: ', entry, 2) for entry in entries if re.compile(pattern)]
+for item in iplist:
+    for p in item[1]:
+        if isinstance(p, str):
+            match = re.search(pattern, p)
+            if match:
+                ipv4list.append((item[0], p))
+        else:
+            print(p)
+print(sorted(ipv4list))
+print('--------- ipv4 addrs ------------ ')
+print(f'{len(ipv4list)} servers with ipv4 addrs.')
 idx = 0
 stamps, server_list = [], []
-for x in newList:
-    if 'DNSCrypt' in x[2]:
-        stamps.append('[static.\'{}-{}\'],stamp = {}'.format(
-          x[0], idx, repr(x[1]), end=' '))
-        server_list.append(('{}-{}'.format(x[0], idx)))
+
+for x in filterTrue:
+#    if 'DoH' in x[2]:
+#        if 'China' in x[0]:
+#            print('cool')
+#        stamps.append('[static.\'{}-{}\'],stamp = {}'.format(
+#          x[0], idx, repr(x[1]), end=' '))
+#        server_list.append(('{}-{}'.format(x[0], idx)))
 #        idx = idx + 1
+#
+    if 'DNSCrypt' in x[2]:
+            idx=0
+            stamps.append('[static.\'{}-{}\'],stamp = {}'.format(
+              x[0], idx, repr(x[1]), end=' '))
+            server_list.append(('{}-{}'.format(x[0], idx)))
+#            idx = idx + 1
+#
+
 slist_str = repr([format(x) for x in server_list[:]][:])
 s = repr(stamps)
 
