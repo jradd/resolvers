a6e4ba7a4afc9035e10796c998db0ea176bcab23
jradd <jredd42@gmail.com>
2020-08-08 17:20:15 -0600

    simpleclassicugly

--- simple_http.py
+++ simple_http.py
@@ -1,12 +1,19 @@
 "Simple=HTTP Methods"
 from socket import socket, getaddrinfo, AF_INET
-from ssl import create_default_context
+import ssl
 import os.path
+import socket 
+
 
 def http_get(url, overwrite=False):
     """ simple http method get request """
     _, _, host, path = url.split('/', 3)
     filename = url.split('/')[-1]
+    if 'https' in url:
+      port = 443
+    else:
+      port = 80
+    addr = getaddrinfo(host, port)[0][-1]
     if os.path.exists(os.path.abspath(os.path.join(os.path.curdir, filename))):
         if overwrite:
             print('overwriting...\r\n')
@@ -16,33 +23,42 @@ def http_get(url, overwrite=False):
             filename = filename + 'new'
             print('writing to: %s\r\n' % filename)
 
-    if 'https:' in url:
-        port = 443
-    elif 'http:' in url:
-        port = 80
+#    conn = socket.socket()
+    addr = getaddrinfo(host, port)[0][-1]
+#    context = ssl.create_default_context()
+#    context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    with socket.create_connection((host, 443)) as sock:
+      with context.wrap_socket(sock, server_hostname=host) as ssock:
+        print(ssock.version())
+#        conn = ssock.connect((host, port), 1024)
+        #ssock.connect(1024)
+        ssock.sendall(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))
+        with open(filename, 'wb') as f:
+            while True:
+                data = ssock.recv(1024)
+                if data:
+                      f.write(bytes(str(data), encoding='utf8', errors='ignore'))
+                else:
+                    break
+#    conn.close()
+#        context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+#        context.verify_mode = ssl.CERT_REQUIRED
+#        context.check_hostname = True
+#        context.load_verify_locations('../certs/demoCA/private/cacert-test-tsakey.pem')
+#        context.load_cert_chain(certfile="../certs/cert.pem", keyfile="../certs/key.pem")
+#        context.load_verify_locations("../certs/demoCA/localhost.crt")
+#        context.load_default_certs()
 
-    if port == 443:
-        context = create_default_context()
-        addr = getaddrinfo(host, port)[0][-1]
-        conn = context.wrap_socket(socket(AF_INET), server_hostname=host)
-    elif port == 80:
-        addr = getaddrinfo(host, port)[0][-1]
-        conn = socket()
+       # ssl_sock = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=host)
+ #       ssock.connect((host, port))
 
-    conn.connect(addr)
-    conn.sendall(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))
-    with open(filename, 'w') as f:
-        while True:
-            data = conn.recv(100)
-            if data:
-                f.write(str(bytes(data), 'utf8'))
-            else:
-                break
-    conn.close()
+#        conn = context.wrap_socket(socket(AF_INET), server_hostname=host)
+
 
 if __name__ == '__main__':
     try:
-        import sys.argv
-        http_get(sys.argv[1])
+        from sys import argv
+        http_get(argv[1])
     except ImportError:
         http_get('https://www.python.org/')
